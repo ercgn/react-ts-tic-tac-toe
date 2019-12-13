030668835ca89667f4c66640aac7b8b12d2a2d11
Eric <eric@duolingo.com>
2019-12-13 14:08:14 -0500

    Add boilerplate components for tic-tac-toe

---
+++ src/components/Square.tsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export class Square extends Component {
+  public render() {
+    return <button className="square">{/* TODO */}</button>;
+  }
+}
+
+export default Square;
