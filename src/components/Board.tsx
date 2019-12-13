41ade25d03e6261a9ed1b8f9f048bab28bb40a51
Eric <eric@duolingo.com>
2019-12-13 16:35:22 -0500

    Fix function permissions

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -2,30 +2,30 @@ import React, { Component } from 'react';
 import { Square } from './Square';
 
 export class Board extends Component {
-  renderSquare(i: number) {
+  private _renderSquare(i: number) {
     return <Square />;
   }
 
-  render() {
+  public render() {
     const status = 'Next player: X';
 
     return (
       <div>
         <div className="status">{status}</div>
         <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
+          {this._renderSquare(0)}
+          {this._renderSquare(1)}
+          {this._renderSquare(2)}
         </div>
         <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
+          {this._renderSquare(3)}
+          {this._renderSquare(4)}
+          {this._renderSquare(5)}
         </div>
         <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
+          {this._renderSquare(6)}
+          {this._renderSquare(7)}
+          {this._renderSquare(8)}
         </div>
       </div>
     );
